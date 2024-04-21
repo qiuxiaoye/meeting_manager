cce87d5b37fe2fd24b87b4d77dbce6913cf496d5
QIU XIAOYE <qiuxiaoye@hotmail.com>
2024-04-21 21:49:44 +0800

    check in

--- meetings/urls.py
+++ meetings/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create_meeting, name='create_meeting'),
-    path('roll_call/<int:meeting_id>/', views.roll_call, name='roll_call'),
+    path('roll_call/', views.roll_call, name='roll_call'),
+    # path('roll_call/<int:meeting_id>/', views.roll_call, name='roll_call'),
     path('minutes/<int:meeting_id>/', views.meeting_minutes, name='meeting_minutes'),
 ]
